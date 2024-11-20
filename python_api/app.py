41dd69e15f502a11e2cc600009bed37c372bb3b4
Orfeu <darkarontg@gmail.com>
2024-11-20 20:59:29 -0300

    configure something on dockerfile and flask

--- python_api/app.py
+++ python_api/app.py
@@ -1,15 +1,21 @@
+import requests
 from flask import Flask
 from flask_cors import CORS
 
 
 DEBUG_MODE = False # Defina como True para habilitar o modo de depuração
 
+
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/items", methods=["GET"])
+def list_items():
+    pass
+
 @app.route('/items/<id>', methods=['GET'])
 def get_item(id):
-    return "jorge"
+    pass
 
 if __name__ == '__main__':
     app.run(debug=DEBUG_MODE)
