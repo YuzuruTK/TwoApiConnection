2ac4d5f37915b967f92325d51a81fc80a345936c
orfeu <darkarontg@gmail.com>
2024-11-20 15:49:55 -0300

    Get Function on Go made

--- go_api/handler.go
+++ go_api/handler.go
@@ -2,66 +2,208 @@
 package main
 
 import (
-    "encoding/json"
-    "net/http"
-    "github.com/gorilla/mux"
+	"encoding/csv"
+	"encoding/json"
+	"log"
+	"math/rand"
+	"net/http"
+	"os"
+	"time"
 )
 
-func getBooks(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(books)
+func getHuman(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(generateRandomHuman())
+}
+func getBeast(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(generateRandomBeast())
 }
 
-func getBook(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
+/*************  ✨ Codeium Command ⭐  *************/
+// generateRandomHuman returns a randomly generated HumanMonster based on
+// the values in the "human.csv" file. The generated HumanMonster will have
+// a randomly selected disposition, vitality, strength, willpower, dextery,
+// and goals. The armor, weapon, trait, ring, and trinket will also be
+// randomly selected based on the values in the "human.csv" file.
+/******  bff20cd9-0ba9-4cf9-8dfb-2ee0021890ba  *******/
+func generateRandomHuman() HumanMonster {
+	csv_file := "human.csv"
+	file, err := os.Open("csv/" + csv_file)
 
-    for _, item := range books {
-        if item.ID == params["id"] {
-            json.NewEncoder(w).Encode(item)
-            return
-        }
-    }
-    w.WriteHeader(http.StatusNotFound)
-    json.NewEncoder(w).Encode(map[string]string{"error": "Book not found"})
+	if err != nil {
+		log.Fatal(err)
+		file.Close()
+	}
+
+	dispositions := []string{"Friendly", "Curious", "Indifferent", "Aggressive", "Hostile"}
+	goals := []string{"Help the most noble Traveler.", "Recruit forces to hunt down a fearsome local legend.", "Find their way back to their lost Safe Haven.", "Understand the nature of a set of ruins.", "Defeat the most infamous Traveler."}
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	disposition := dispositions[rand.Intn(len(dispositions))]
+	goal := goals[rand.Intn(len(goals))]
+
+	status := []int{}
+	switch rand.Intn(5) {
+	case 0:
+		status = []int{1, 2, 0, 1}
+	case 1:
+		status = []int{rollDice(10, 1), 4, 0, 1}
+	case 2:
+		status = []int{rollDice(10, 2), 6, 1, 2}
+	case 3:
+		status = []int{rollDice(10, 3), 8, 1, 2}
+	case 4:
+		status = []int{rollDice(10, 4), 10, 2, 4}
+	}
+
+	typesOfFinger := []string{"LL", "LA", "LW", "AL", "AA", "AW", "WL", "WA", "WW"}
+
+	thumb := typesOfFinger[rollDice(9, 1)-1]
+	pointer := typesOfFinger[rollDice(9, 1)-1]
+	middle := typesOfFinger[rollDice(9, 1)-1]
+	ringy := typesOfFinger[rollDice(9, 1)-1]
+	little := typesOfFinger[rollDice(9, 1)-1]
+
+	Armor := ""
+	Weapon := ""
+	Trait := ""
+	Ring := ""
+	Trinket := ""
+
+	for _, item := range records {
+		if item[0] == thumb && item[1] == "Armor" {
+			Armor = item[2]
+		}
+		if item[0] == pointer && item[1] == "Weapon" {
+			Weapon = item[2]
+		}
+		if item[0] == middle && item[1] == "Trait" {
+			Trait = item[2]
+		}
+		if item[0] == ringy && item[1] == "Ring" {
+			Ring = item[2]
+		}
+		if item[0] == little && item[1] == "Trinket" {
+			Trinket = item[2]
+		}
+	}
+
+	return HumanMonster{
+		Disposition: disposition,
+		Vitality:    status[0],
+		Strength:    status[1],
+		Willpower:   status[2],
+		Dextery:     status[3],
+		Goals:       goal,
+		Armor:       Armor,
+		Weapon:      Weapon,
+		Trait:       Trait,
+		Ring:        Ring,
+		Trinket:     Trinket,
+	}
 }
 
-func createBook(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    var book Book
-    _ = json.NewDecoder(r.Body).Decode(&book)
-    books = append(books, book)
-    json.NewEncoder(w).Encode(book)
+func generateRandomBeast() BeastMonster {
+	csv_file := "beast.csv"
+	file, err := os.Open("csv/" + csv_file)
+
+	if err != nil {
+		log.Fatal(err)
+		file.Close()
+	}
+	dispositions := []string{"Friendly", "Curious", "Indifferent", "Aggressive", "Hostile"}
+	goals := []string{"Mark their territory", "Defend their young from hunters", "Hunt for food.", "Flee from impending ecological disaster", "Find their missing brood"}
+	status := []int{}
+
+	randomNumber := rand.Intn(5)
+	switch {
+	case randomNumber == 0:
+		status = []int{1, 2, 0, 1}
+	case randomNumber == 1:
+		status = []int{rollDice(10, 1), 4, 0, 1}
+	case randomNumber == 2:
+		status = []int{rollDice(10, 2), 6, 1, 2}
+	case randomNumber == 3:
+		status = []int{rollDice(10, 3), 8, 1, 2}
+	case randomNumber == 4:
+		status = []int{rollDice(10, 4), 10, 2, 4}
+	}
+
+	typesOfFinger := []string{"LL", "LA", "LW", "AL", "AA", "AW", "WL", "WA", "WW"}
+
+	thumb := typesOfFinger[rollDice(9, 1)-1]
+	pointer := typesOfFinger[rollDice(9, 1)-1]
+	middle := typesOfFinger[rollDice(9, 1)-1]
+	ringy := typesOfFinger[rollDice(9, 1)-1]
+	little := typesOfFinger[rollDice(9, 1)-1]
+
+	// Roll dice
+	Disposition := dispositions[rand.Intn(5)]
+	Vitality := status[0]
+	Strength := status[1]
+	Willpower := status[2]
+	Dextery := status[3]
+	Goal := goals[rand.Intn(4)]
+	// Get from CSV
+	Armor := ""
+	Attack := ""
+	Trait := ""
+	Trait2 := ""
+	Ability := ""
+
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, item := range records {
+		if item[0] == thumb && item[1] == "Armor" {
+			Armor = item[2]
+		}
+		if item[0] == pointer && item[1] == "Attack" {
+			Attack = item[2]
+		}
+		if item[0] == middle && item[1] == "Trait" {
+			Trait = item[2]
+		}
+		if item[0] == ringy && item[1] == "Trait2" {
+			Trait2 = item[2]
+		}
+		if item[0] == little && item[1] == "Ability" {
+			Ability = item[2]
+		}
+	}
+	return BeastMonster{
+		Disposition: Disposition,
+		Vitality:    Vitality,
+		Strength:    Strength,
+		Willpower:   Willpower,
+		Dextery:     Dextery,
+		Desires:     Goal,
+		Armor:       Armor,
+		Attack:      Attack,
+		Trait:       Trait,
+		Trait2:      Trait2,
+		Ability:     Ability,
+	}
 }
 
-func updateBook(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
-    var updatedBook Book
-    _ = json.NewDecoder(r.Body).Decode(&updatedBook)
-
-    for i, item := range books {
-        if item.ID == params["id"] {
-            books[i] = updatedBook
-            json.NewEncoder(w).Encode(updatedBook)
-            return
-        }
-    }
-    w.WriteHeader(http.StatusNotFound)
-    json.NewEncoder(w).Encode(map[string]string{"error": "Book not found"})
-}
-
-func deleteBook(w http.ResponseWriter, r *http.Request) {
-    w.Header().Set("Content-Type", "application/json")
-    params := mux.Vars(r)
-
-    for i, item := range books {
-        if item.ID == params["id"] {
-            books = append(books[:i], books[i+1:]...)
-            json.NewEncoder(w).Encode(map[string]string{"message": "Book deleted"})
-            return
-        }
-    }
-    w.WriteHeader(http.StatusNotFound)
-    json.NewEncoder(w).Encode(map[string]string{"error": "Book not found"})
+func rollDice(d int, s int) int {
+	rand.Seed(time.Now().UnixNano())
+	dice := 0
+	for i := 0; i < s; i++ {
+		dice += rand.Intn(d) + 1
+	}
+	return dice
 }
--- go_api/model.go
+++ go_api/model.go
@@ -2,14 +2,36 @@
 package main
 
 type Book struct {
-    ID     string `json:"id"`
-    Title  string `json:"title"`
-    Author string `json:"author"`
-    Year   int    `json:"year"`
+	ID     string `json:"id"`
+	Title  string `json:"title"`
+	Author string `json:"author"`
+	Year   int    `json:"year"`
 }
 
-// In-memory data store
-var books = []Book{
-    {ID: "1", Title: "1984", Author: "George Orwell", Year: 1949},
-    {ID: "2", Title: "To Kill a Mockingbird", Author: "Harper Lee", Year: 1960},
+type HumanMonster struct {
+	Disposition string `json:"disposition"`
+	Vitality    int    `json:"vitality"`
+	Strength    int    `json:"strength"`
+	Willpower   int    `json:"willpower"`
+	Dextery     int    `json:"dextery"`
+	Goals       string `json:"goals"`
+	Armor       string `json:"armor"`
+	Weapon      string `json:"weapon"`
+	Trait       string `json:"trait"`
+	Ring        string `json:"ring"`
+	Trinket     string `json:"trinket"`
+}
+
+type BeastMonster struct {
+	Disposition string `json:"disposition"`
+	Vitality    int    `json:"vitality"`
+	Strength    int    `json:"strength"`
+	Willpower   int    `json:"willpower"`
+	Dextery     int    `json:"dextery"`
+	Desires     string `json:"desires"`
+	Armor       string `json:"armor"`
+	Attack      string `json:"attack"`
+	Trait       string `json:"trait"`
+	Trait2      string `json:"trait2"`
+	Ability     string `json:"ability"`
 }
